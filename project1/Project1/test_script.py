1ba2bf13a4623899aeda070c9f9268fa65bf270f
Kevin Ye <ranchonpizza24@gmail.com>
2025-09-23 14:58:25 -0700

    chore: misc commits

--- project1/Project1/test_script.py
+++ project1/Project1/test_script.py
@@ -1,7 +1,7 @@
 import numpy as np
 import decision_trees as dt
 import nearest_neighbors as nn
-import clustering as kmeans
+# import clustering as kmeans
 
 def load_data(fname):
   f = open(fname, 'r')
@@ -39,11 +39,11 @@ X,Y = load_data("data_4.txt")
 acc = nn.KNN_test(X,Y,X,Y,1)
 print("KNN:", acc)
 
-X,Y = load_data("data_5.txt")
-mu = np.array([[1],[5]])
-mu = kmeans.K_Means(X,2,mu)
-print("KMeans:",mu)
+# X,Y = load_data("data_5.txt")
+# mu = np.array([[1],[5]])
+# mu = kmeans.K_Means(X,2,mu)
+# print("KMeans:",mu)
 
-X,Y = load_data("data_6.txt")
-mu = kmeans.K_Means(X,2,[])
-print("KMeans:",mu)
+# X,Y = load_data("data_6.txt")
+# mu = kmeans.K_Means(X,2,[])
+# print("KMeans:",mu)
