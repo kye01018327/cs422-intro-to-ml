e0bbcada0f97ff3794856d3e6a71bf9e29f2c126
Kevin Ye <ranchonpizza24@gmail.com>
2025-10-18 00:12:43 -0700

    docs(perceptron): add pseudocode and docstring for perceptron_train function

--- Project2/perceptron.py
+++ Project2/perceptron.py
@@ -8,11 +8,34 @@ print(load_data(f'data_{num}.txt'))
 
 # %%
 def perceptron_train(X, Y):
-    # Initialize weight vector and bias to 0
-        # Initialize weight vector 
-    w, b = 0, 0
+    """
+    Train perceptron.
+
+    Pseudocode:
+    # Initialize weights and bias
+        # Initialize weights vector to have same dimension as feature vector, all initial values as 0
+        # Initialize bias as a scalar with initial value of 0
+    # Until convergence (no change over one epoch), train weights and bias
+        # For each feature vector in the feature dataset,
+            # Calculate the activation
+                # This being the sum of the product of the weight vector and feature vector, plus the bias
+            # Evaluate whether the activation aligns with the label (signed margin)
+            # If signed margin is negative (activation is opposite direction of label), adjust weights and bias
+                # if signed margin is negative (ya <= 0)
+                    # w = w + yx
+                    # b = b + y
+                    # Track that a change occurred in this epoch
+        # If no change occurred in epoch, return weight vector and bias
+        # Keep track of number of epochs, if epochs exceed a limit, throw error
+    Return:
+        w: ndarray
+            trained weight vector
+        b: float
+            trained bias
+    """
     
-    return w, b
+
+    pass
 
 # %%
 def perceptron_test(X_test, Y_test, w, b):
