b3e31162ca67cf39da79527e136b4720e69e8a1d
Kevin Ye <ranchonpizza24@gmail.com>
2025-10-17 23:50:51 -0700

    chore: initialize project structure

---
+++ Project2/perceptron.py
@@ -0,0 +1,20 @@
+# %%
+import numpy as np
+from utils import load_data
+
+# %%
+num = 1
+print(load_data(f'data_{num}.txt'))
+
+# %%
+def perceptron_train(X, Y):
+    # Initialize weight vector and bias to 0
+        # Initialize weight vector 
+    w, b = 0, 0
+    
+    return w, b
+
+# %%
+def perceptron_test(X_test, Y_test, w, b):
+    pass
+
