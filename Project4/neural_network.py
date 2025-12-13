49474bfb4a5e614f53ecddbec9c899c2af5847c6
Kevin Ye <ranchonpizza24@gmail.com>
2025-12-12 17:03:22 -0800

    chore: construct and display data

--- Project4/neural_network.py
+++ Project4/neural_network.py
@@ -1,3 +1,11 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import make_moons
+np.random.seed(0)
+X, y = make_moons(200, noise=0.2)
+
+
 # Helper function to evaluate the total loss on the dataset
 # model is the current version of the model
 # {
@@ -8,5 +16,18 @@
 # }
 # X is all the training labels
 # y is the training labels
+
+# %%
 def calculate_loss(model: dict, X, y):
+    N = len(X)
     pass
+
+
+# %%
+print(X)
+print(y)
+
+
+# %%
+# Display data
+plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
