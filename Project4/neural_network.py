188dc9f81ec54b1081dc48da323f0c90e531623d
Kevin Ye <ranchonpizza24@gmail.com>
2025-12-12 16:48:27 -0800

    chore: add stub for calculate_loss helper function

---
+++ Project4/neural_network.py
@@ -0,0 +1,12 @@
+# Helper function to evaluate the total loss on the dataset
+# model is the current version of the model
+# {
+#     'W1': W1,
+#     'b1': b1,
+#     'W2': W2,
+#     'b2': b2
+# }
+# X is all the training labels
+# y is the training labels
+def calculate_loss(model: dict, X, y):
+    pass
