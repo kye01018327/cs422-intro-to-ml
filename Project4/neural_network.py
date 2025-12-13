1bf4bae61030b57bd80f7bc908e2290164db3670
Kevin Ye <ranchonpizza24@gmail.com>
2025-12-12 23:23:54 -0800

    feat: add initial implementation of loss (untested)

--- Project4/neural_network.py
+++ Project4/neural_network.py
@@ -18,16 +18,37 @@ X, y = make_moons(200, noise=0.2)
 # y is the training labels
 
 # %%
-def calculate_loss(model: dict, X, y):
+def calculate_loss(model: dict, X: np.ndarray, Y: np.ndarray):
+    def softmax(Z):
+        Z_stable = Z - np.max(Z, axis=1, keepdims=True)
+        exp_Z = np.exp(Z_stable)
+        sum_exp_Z = np.sum(exp_Z, axis=1, keepdims=True)
+        Y_hat = exp_Z / sum_exp_Z
+        return Y_hat
+    
+    # Calculate prediction matrix
+    A = X @ model['W1'] + model['b1']
+    H = np.tanh(A)
+    Z = H @ model['W2'] + model['b2']
+    Y_hat = softmax(Z)
+
     N = len(X)
-    pass
+    epsilon = 1e-12
+    loss = np.sum(Y * np.log(Y_hat + epsilon), axis=1)
+    loss = np.sum(loss)
+    loss = -loss / N
+    return loss
 
 
 # %%
-print(X)
-print(y)
+print(len(y))
+print(len(X[0]))
+
 
 
 # %%
 # Display data
 plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
+
+
+# %%
