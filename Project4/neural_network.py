b1c6cf7f122dcd80240c6c27f8459ba76508b979
Kevin Ye <ranchonpizza24@gmail.com>
2025-12-12 23:38:48 -0800

    feat: add predict function (untested)

--- Project4/neural_network.py
+++ Project4/neural_network.py
@@ -6,25 +6,17 @@ np.random.seed(0)
 X, y = make_moons(200, noise=0.2)
 
 
-# Helper function to evaluate the total loss on the dataset
-# model is the current version of the model
-# {
-#     'W1': W1,
-#     'b1': b1,
-#     'W2': W2,
-#     'b2': b2
-# }
-# X is all the training labels
-# y is the training labels
+# %%
+def softmax(Z):
+    Z_stable = Z - np.max(Z, axis=1, keepdims=True)
+    exp_Z = np.exp(Z_stable)
+    sum_exp_Z = np.sum(exp_Z, axis=1, keepdims=True)
+    Y_hat = exp_Z / sum_exp_Z
+    return Y_hat
+
 
 # %%
 def calculate_loss(model: dict, X: np.ndarray, Y: np.ndarray):
-    def softmax(Z):
-        Z_stable = Z - np.max(Z, axis=1, keepdims=True)
-        exp_Z = np.exp(Z_stable)
-        sum_exp_Z = np.sum(exp_Z, axis=1, keepdims=True)
-        Y_hat = exp_Z / sum_exp_Z
-        return Y_hat
     
     # Calculate prediction matrix
     A = X @ model['W1'] + model['b1']
@@ -38,6 +30,17 @@ def calculate_loss(model: dict, X: np.ndarray, Y: np.ndarray):
     loss = np.sum(loss)
     loss = -loss / N
     return loss
+
+
+# %%
+def predict(model, x):
+    a = x @ model['W1'] + model['b1']
+    h = np.tanh(a)
+    z = h @ model['W2'] + model['b2']
+    y_hat = softmax(z)
+    return np.argmax(y_hat)
+
+
 
 
 # %%
